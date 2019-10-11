99e653a534dfe8cbbdc22f5c2011764789179ac0
zhangwei <zhangwei0659@cairenhui.com>
2019-10-11 11:12:02 +0800

    '项目初始化'

---
+++ src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <a>{{ msg }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '首页'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.container {
+  display: inline-block;
+  background: #fff;
+    border-radius: 3px;
+    font-size: 36px;
+    font-weight: 600;
+    padding: 5px 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    // {  //重定向
+    //   path: '/',
+    //   redirect: '/homePage'
+    // },
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      // @代表src目录
+      component: () => import('@/components/HelloWorld.vue'),
+      meta: {
+        title: '测试页'
+      }
+    }, {
+      path: '/home',
+      name: 'home',
+      component: () => import('@/pages/home.vue'),
+      meta: {
+        title: '首页'
+      }
     }
   ]
 })
