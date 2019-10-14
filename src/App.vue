8745df52e2088e44c583c68c5de72c9a84fcc0d4
zhangwei <zhangwei0659@cairenhui.com>
2019-10-14 17:24:34 +0800

    使用‘keep-alive’优化网页性能

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- keep-alive为vue自带标签，路由的内容被加载一次之后就会将其放在内存之中；下次再进这个路由时不需要再去重新渲染钩子函数 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
