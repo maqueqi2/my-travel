aa325bbfbd7e503c74cee495ccc935dc423363c4
zhangwei <zhangwei0659@cairenhui.com>
2019-10-15 16:17:02 +0800

    递归组件

--- src/pages/detail/components/header.vue
+++ src/pages/detail/components/header.vue
@@ -69,6 +69,7 @@ export default {
     top 0
     left 0
     right 0
+    z-index 9
     height $headerHeight
     line-height $headerHeight
     text-align center
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,18 +2,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/banner'
 import detailHeader from './components/header'
+import detailList from './components/list'
 export default {
   name: 'detail',
   components: {
     detailBanner,
-    detailHeader
+    detailHeader,
+    detailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '学生票'
+      }, {
+        title: '成人票',
+        chilren: [{
+          title: '成人三馆联票',
+          chilren: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
